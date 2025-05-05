557076efd88d7663b4f7d67efd44a5428da8b02e
Kath-Fernandes <kathleen.fernandes92@gmail.com>
2025-05-05 00:25:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let botao = document.getElementById("meuBotao");
+let mensagem = document.getElementById("mensagem");
+
+botao.addEventListener("click", function () {
+  mensagem.innerHTML = "Não tenha medo de recomeçar. Você pode criar algo ainda melhor.✨";
+});
+
